e299a4a054774c543b4c84050dc1624fd5517cc2
Kai Page <kaibug@gmail.com>
2019-09-20 21:02:50 -0500

    Simple navigation layout

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -3,6 +3,6 @@ import 'package:angular/angular.dart';
 @Component(
   selector: "bd-app",
   templateUrl: "app_component.html",
-  styleUrls: ["app_component.css"],
+  styleUrls: ["app_component.css", "material-icons.css"],
 )
 class AppComponent {}
